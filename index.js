0c0e77e1b40713f24b0f7e9ed95eebb6463ba161
Brendon Barreto <brendondominic@gmail.com>
2019-08-07 09:19:00 -0300

    Removing programatically build process

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const chalk = require('chalk')
-const build = require('./build')
 const runServer = require('./runServer')
 
 module.exports = testOutput => {
-  build()
-    .then(() => {
-      console.log(chalk.green('Build succeeded'));
-      runServer(testOutput)
-    })
-    .catch(e => console.log(chalk.red(e)))
+  runServer(testOutput)
 }
