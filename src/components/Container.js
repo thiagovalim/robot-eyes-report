f09af4a4ccef5e3881df74eefebbc03201c88cbc
Brendon Barreto <brendondominic@gmail.com>
2019-09-13 11:10:15 -0300

    Setting DIFF as default display option

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,8 +6,6 @@ import ViewportButtons from './ViewportButtons'
 import DisplayOptionsEnum from '../DisplayOptionEnum'
 import DiffContainer from './DiffContainer'
 
-// import getFailedTests from '../getFailedTests'
-
 import './Container.scss'
 
 import Empty from "./Empty"
@@ -25,20 +23,11 @@ class Container extends Component {
         ...this.setSelectedViewports(failedTests).map(a => {
           return {
             ...a,
-            display: DisplayOptionsEnum.SIDE_BY_SIDE
+            display: DisplayOptionsEnum.DIFF
           }
         }),
       ]
     }
-    // try {
-    //   this.state = {
-    //     failedTests: this.setSelectedViewports(getFailedTests())
-    //   }
-    // } catch (e) {
-    //   this.state = {
-    //     failedTests: this.setSelectedViewports(this.shu())
-    //   }
-    // }
   }
 
   setSelectedViewports = failedTests =>
@@ -65,54 +54,6 @@ class Container extends Component {
     this.setState({
       failedTests
     })
-  }
-
-
-  shu = () => {
-    return [
-      {
-        name: 'listar_clientes',
-        viewports: [
-          {
-            name: "1920x1080",
-            width: 1920,
-            height: 1080,
-            testImage: "https://s2.best-wallpaper.net/wallpaper/1920x1080/1508/Futuristic-city-night-lights_1920x1080.jpg",
-            diffImage: "https://www.10wallpaper.com/wallpaper/1920x1080/1712/Hong_Kong_modern_architecture_night_photography_4K_HD_1920x1080.jpg",
-            referenceImage: "https://media-manager.noticiasaominuto.com/1920/1535456391/naom_5b8532e98ea35.jpg"
-          },
-          {
-            name: "1366x768",
-            width: 1366,
-            height: 768,
-            testImage: "https://s2.best-wallpaper.net/wallpaper/1366x768/1803/Futuristic-city-fantasy-buildings-spaceships_1366x768.jpg",
-            diffImage: "https://www.10wallpaper.com/wallpaper/1366x768/1601/Modern_Cities_Night_Landscape_HD_Wallpapers_1366x768.jpg",
-            referenceImage: "https://www.10wallpaper.com/wallpaper/1366x768/1610/Sky_view_of_an_old_city-Cities_Corner_HD_Wallpaper_1366x768.jpg"
-          }
-        ]
-      },
-      {
-        name: 'Carros',
-        viewports: [
-          {
-            name: "1920x1080",
-            width: 1920,
-            height: 1080,
-            testImage: "https://media-manager.noticiasaominuto.com.br/1920/1492611576/naom_58f7718612d2d.jpg",
-            diffImage: "https://cdn.autopapo.com.br/box/uploads/2018/12/18151806/volkswagen-polo-e-virtus-sense-pcd1.jpg",
-            referenceImage: "https://conteudo.imguol.com.br/c/entretenimento/a4/2016/05/12/fiat-1100-de-1946-de-propriedade-de-luiz-pixoxo-e-anderson-albertini-de-campinas-sp-no-10-encontro-dos-amigos-do-carro-antigo-de-jaguariuna-sp-1463106856568_1920x1080.png"
-          },
-          {
-            name: "1366x768",
-            width: 1366,
-            height: 768,
-            testImage: "http://planetcarsz.com/assets/uploads/b24cda49d90f521faa75eb5a42f4392d.jpg",
-            diffImage: "https://carros.hi7.co/carros/carros-56653506e9dbf.jpg",
-            referenceImage: "https://carrosantigos.blog.br/wp-content/uploads/carros-antigos-blog-ford-hot-rod-ano-1932.jpg"
-          }
-        ]
-      }
-    ]
   }
 
   changeDisplayOption = (failedTest, selectedOption) => {
